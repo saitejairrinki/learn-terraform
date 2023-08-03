c75d00e3f645bc9d5431cf3e15f39459eb66016a
Raghu K <raghuk.vit@gmail.com>
2023-08-03 06:35:45 +0530

    Terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,8 +1,45 @@
+# Plain Variable
 variable "fruit_name" {
   default = "apple"
 }
 
 output "fruit_name" {
   value = var.fruit_name
+}
+
+# List Variable
+variable "fruits" {
+  default = [
+    "apple",
+    "banana"
+  ]
+  // default = [ "apple", "banana" ] // Single line syntax
+}
+
+# Map Variable , Plain
+variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 200
+  }
+}
+
+# Map Variable, Map of Maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 3
+    }
+    banana = {
+      stock = 400
+      price = 1
+    }
+  }
+}
+
+## Access a list variable, List index starts from zero
+output "fruits_first" {
+  value = var.fruits[0]
 }
 
---
+++ roboshop-v2/main.tf
@@ -0,0 +1,14 @@
+//variable "components" {
+//  default = [ "frontend", "mongodb", "catalogue", ]
+//}
+
+//variable "components" {
+//  default = {
+//    frontend = {
+//      instance_type = "t3.micro"
+//    }
+//    mongodb = {
+//      instance_type = "t3.small"
+//    }
+//  }
+//}
