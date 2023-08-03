e6ec40fdfd91f1a763c13d3ad6a4566cd04e3349
Raghu K <raghuk.vit@gmail.com>
2023-08-03 06:55:43 +0530

    Terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -55,3 +55,25 @@ output "fruit_stock_apple" {
 output "fruit_stock_with_price_of_apple" {
   value = var.fruit_stock_with_price["apple"].stock
 }
+
+# Variable Data Types
+variable "fruit_details" {
+  default = {
+    apple = {
+      stock = 100  # number
+      type = "washington" # string
+      for_sale = true  # boolean
+    }
+  }
+}
+
+# Variable in a combination of any other string then it needs to be with in  ${}
+output "fruit_name_1" {
+  value = "Fruit Name = ${var.fruit_name}"
+}
+
+
+output "fruit_details_apple" {
+  value = "Apple Stock = ${var.fruit_details["apple"].stock} , Apple Type = ${var.fruit_details["apple"].type}, Apple Sale Status = ${var.fruit_details["apple"].for_sale}"
+}
+
