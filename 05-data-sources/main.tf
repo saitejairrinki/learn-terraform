131306a302cc70e5feaf247d805dd3d071cf437a
Raghu K <raghuk.vit@gmail.com>
2023-08-03 07:25:40 +0530

    Terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,8 +1,8 @@
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   name_regex       = "Centos-*"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami.ami
+  value = data.aws_ami_ids.ami
 }
