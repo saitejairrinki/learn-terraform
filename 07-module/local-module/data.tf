18d7cffa774abda87e007f816dacca463188a71a
Raghu K <raghuk.vit@gmail.com>
2023-08-05 06:30:44 +0530

    Terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,8 +1,16 @@
-data "aws_ami_ids" "ami" {
-  name_regex       = "Centos-*"
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
+}
+
