99630443f1058c5777000f297661d52552810441
Raghu K <raghuk.vit@gmail.com>
2023-08-05 07:04:18 +0530

    Terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -7,9 +7,6 @@ resource "aws_instance" "instance" {
     Name = var.name
   }
 
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
 }
 
 
@@ -21,3 +18,18 @@ resource "aws_route53_record" "record" {
   records = [ aws_instance.instance.private_ip ]
 }
 
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.rdevopsb72.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
